d26d07b20225e65f2b4f36bc858e60b952a8d557
Phillip Taylor <phil.taylor.sj@gmail.com>
2024-08-11 16:22:12 +0100

    Added tests for vector multiplication in Vec2Int_Tests.

--- test/Vec2Int_Test.cpp
+++ test/Vec2Int_Test.cpp
@@ -38,37 +38,67 @@ namespace Vec2Int_Tests
             Vec2i vectorOne, vectorTwo, expected;
     };
 
-
-    // void operator *= (int value)
-    class MultiplyEqualsScalarF : public VecScaVecFixture {};
-    TEST_P(MultiplyEqualsScalarF, ApplyMultiplyEqualsScalar)
-    {
-        vector *= scalar;
-        ASSERT_EQ(expected.x, vector.x);
-        ASSERT_EQ(expected.y, vector.y);   
-    }
-    
-    
-    INSTANTIATE_TEST_SUITE_P(ApplyMultiplyEquals, MultiplyEqualsScalarF, testing::Values(
-        std::make_tuple(Vec2i(0, 0), 10, Vec2i(0, 0)),
-        std::make_tuple(Vec2i(3, 4), 10, Vec2i(30, 40)),
-        std::make_tuple(Vec2i(-6, 4), 5, Vec2i(-30, 20)),
-        std::make_tuple(Vec2i(8, -7), -5, Vec2i(-40, 35))
-    ));
-
-    // Vec2i operator * (int value)
-    class MultiplyScalarF : public VecScaVecFixture {};
-    TEST_P(MultiplyScalarF, MultiplyScalar)
+    // Vec2i operator * (int scalar) const
+    class MultiplyByScalarF : public VecScaVecFixture {};
+    TEST_P(MultiplyByScalarF, MultiplyByScalar)
     {
         Vec2i output = vector * scalar;
         ASSERT_EQ(expected.x, output.x);
         ASSERT_EQ(expected.y, output.y);   
     }
 
-    INSTANTIATE_TEST_SUITE_P(MultiplyEquals, MultiplyScalarF, testing::Values(
+    INSTANTIATE_TEST_SUITE_P(MultiplyByScalar, MultiplyByScalarF, testing::Values(
         std::make_tuple(Vec2i(0, 0), 10, Vec2i(0, 0)),
         std::make_tuple(Vec2i(3, 4), 10, Vec2i(30, 40)),
         std::make_tuple(Vec2i(-6, 4), 5, Vec2i(-30, 20)),
         std::make_tuple(Vec2i(8, -7), -5, Vec2i(-40, 35))
+    ));
+
+    // Vec2i operator * (const Vec2i& vector) const
+    class MultiplyByVectorF : public VecVecVecFixture {};
+    TEST_P(MultiplyByVectorF, MultiplyByVector)
+    {
+        Vec2i output = vectorOne * vectorTwo;
+        ASSERT_EQ(expected.x, output.x);
+        ASSERT_EQ(expected.y, output.y);   
+    }
+
+    INSTANTIATE_TEST_SUITE_P(MultiplyByVector, MultiplyByVectorF, testing::Values(
+        std::make_tuple(Vec2i(0, 0), Vec2i(10, 10), Vec2i(0, 0)),
+        std::make_tuple(Vec2i(3, 4), Vec2i(5, 10), Vec2i(15, 40)),
+        std::make_tuple(Vec2i(-6, 4), Vec2i(5, -5), Vec2i(-30, -20)),
+        std::make_tuple(Vec2i(8, -7), Vec2i(-5, -5), Vec2i(-40, 35))
+    ));
+
+    // void operator *= (int scalar)
+    class MultiplyEqualsByScalarF : public VecScaVecFixture {};
+    TEST_P(MultiplyEqualsByScalarF, MultiplyEqualsByScalar)
+    {
+        vector *= scalar;
+        ASSERT_EQ(expected.x, vector.x);
+        ASSERT_EQ(expected.y, vector.y);   
+    }
+    
+    INSTANTIATE_TEST_SUITE_P(MultiplyEqualsByScalar, MultiplyEqualsByScalarF, testing::Values(
+        std::make_tuple(Vec2i(0, 0), 10, Vec2i(0, 0)),
+        std::make_tuple(Vec2i(3, 4), 10, Vec2i(30, 40)),
+        std::make_tuple(Vec2i(-6, 4), 5, Vec2i(-30, 20)),
+        std::make_tuple(Vec2i(8, -7), -5, Vec2i(-40, 35))
+    ));
+
+    // void operator *= (const Vec2i& vector)
+    class MultiplyEqualsByVectorF : public VecVecVecFixture {};
+    TEST_P(MultiplyEqualsByVectorF, MultiplyEqualsByVector)
+    {
+        vectorOne *= vectorTwo;
+        ASSERT_EQ(expected.x, vectorOne.x);
+        ASSERT_EQ(expected.y, vectorOne.y);   
+    }
+    
+    INSTANTIATE_TEST_SUITE_P(MultiplyEqualsByVector, MultiplyEqualsByVectorF, testing::Values(
+        std::make_tuple(Vec2i(0, 0), Vec2i(10, 10), Vec2i(0, 0)),
+        std::make_tuple(Vec2i(3, 4), Vec2i(5, 10), Vec2i(15, 40)),
+        std::make_tuple(Vec2i(-6, 4), Vec2i(5, -5), Vec2i(-30, -20)),
+        std::make_tuple(Vec2i(8, -7), Vec2i(-5, -5), Vec2i(-40, 35))
     ));
 }
